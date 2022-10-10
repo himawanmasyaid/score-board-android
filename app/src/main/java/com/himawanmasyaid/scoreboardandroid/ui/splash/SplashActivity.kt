ca84da2b8ac5705667f79eb2a084d4e9058a525b
himawan.masyaid <himawan.masyaid@gmail.com>
2022-10-11 00:03:46 +0700

    - install library lifecycle & ktx, - create ui splash screen - create splash viewmodel & delay time

---
+++ app/src/main/java/com/himawanmasyaid/scoreboardandroid/ui/splash/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.himawanmasyaid.scoreboardandroid.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.himawanmasyaid.scoreboardandroid.common.viewBinding
+import com.himawanmasyaid.scoreboardandroid.databinding.ActivitySplashBinding
+import com.himawanmasyaid.scoreboardandroid.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val binding by viewBinding(ActivitySplashBinding::inflate)
+    private val viewModel by viewModels<SplashViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        startObserve()
+
+    }
+
+    private fun startObserve() {
+
+        viewModel.splashState.observe(this) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+
+}
