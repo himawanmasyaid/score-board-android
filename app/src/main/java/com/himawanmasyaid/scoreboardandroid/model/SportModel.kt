1f6b23a02fe6051715ab475df4fc5679392594b9
himawan.masyaid <himawan.masyaid@gmail.com>
2022-10-11 16:55:18 +0700

    - sport list data - main view model

--- app/src/main/java/com/himawanmasyaid/scoreboardandroid/model/SportModel.kt
+++ app/src/main/java/com/himawanmasyaid/scoreboardandroid/model/SportModel.kt
@@ -1,5 +1,7 @@
 package com.himawanmasyaid.scoreboardandroid.model
 
+import com.himawanmasyaid.scoreboardandroid.R
+
 class SportModel(
     val id: Int,
     val name: String,
@@ -8,3 +10,22 @@ class SportModel(
     val is_have_time_duration: Boolean = false,
     val total_time_duration: Int = 0, // 2000 : 2 second (time millisecond)
 )
+
+fun getSportListData(): List<SportModel> {
+
+    return listOf(
+        SportModel(
+            id = 1,
+            name = "Bulutangkis",
+            total_score = 21,
+            image = R.drawable.ic_launcher_background
+        ),
+        SportModel(
+            id = 2,
+            name = "Tenis Meja",
+            total_score = 12,
+            image = R.drawable.ic_launcher_background
+        ),
+    )
+
+}
--- app/src/main/java/com/himawanmasyaid/scoreboardandroid/ui/main/MainActivity.kt
+++ app/src/main/java/com/himawanmasyaid/scoreboardandroid/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.himawanmasyaid.scoreboardandroid.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import com.himawanmasyaid.scoreboardandroid.R
 import com.himawanmasyaid.scoreboardandroid.common.viewBinding
 import com.himawanmasyaid.scoreboardandroid.databinding.ActivityMainBinding
@@ -10,19 +12,32 @@ import com.himawanmasyaid.scoreboardandroid.ui.base.BaseActivity
 class MainActivity : BaseActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         initView()
+        startObserve()
+        viewModel.getSports()
 
     }
 
     private fun initView() {
 
+    }
 
+    private fun startObserve() {
 
+        viewModel.sportState.observe(this) {
+            setLog("total data : ${it.size}")
+        }
+
+    }
+
+    private fun setLog(msg: String) {
+        Log.e("main", msg)
     }
 
 }
